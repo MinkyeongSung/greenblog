4071f939aeea1090fe3344be31f3ad73cb440a8e
heidi505 <xxwhite19@gmail.com>
2023-08-25 11:34:22 +0900

    깃헙테스트2

--- src/main/java/shop/mtcoding/blogv2/user/UserController.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
-
+    public static void main(String[] args) {
+        System.out.println("-----");
+    }
   
 }
